ce5dc724a73b501d4799054450cc8a0f60d9d3e0
Mika Vainio <koti.mika.vainio@gmail.com>
2020-09-10 14:10:59 +0300

    Lisätty Jäsen-luokka

--- Sali3/Program.cs
+++ Sali3/Program.cs
@@ -8,9 +8,28 @@ namespace Sali3
         {
             Console.WriteLine("Testataan Henkilo-olion luontia");
 
+            /*--------------------------------------------------
+             | TESTAUS ALKAA TÄSTÄ                              |
+             |--------------------------------------------------*/
+
             // Testataan oletusmuodostin
 
-            Henkilo henkilo =
+            Henkilo henkilo = new Henkilo();
+
+            // Testataan muodostin kaikilla parametreillä
+
+            Henkilo henkilo1 = new Henkilo("Mika", "Hakala", "Kotikatu 3", "20100", "Turku");
+
+            // Testataan Jäsen-olion muodostimen toiminta
+            Jasen jasen = new Jasen("Mika", "Vainio", "Yläneentie 146", "23100", "Mynämäki", "123", "1");
+            
+            // Katsotaan olion ominaisuuksia
+            Console.WriteLine("Testi oletusmuodostimelle");
+            henkilo.naytaHenkilo();
+            Console.WriteLine("Testi muodostimelle kaikilla param");
+            henkilo1.naytaHenkilo();
+            Console.WriteLine("Testi jäsenen muodostimelle");
+            jasen.naytaJasen();
         }
     }
 }
