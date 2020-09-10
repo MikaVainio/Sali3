429f9f7dd720b42cee17e94164e172f63798a91b
Mika Vainio <koti.mika.vainio@gmail.com>
2020-09-10 15:56:06 +0300

    Lisätty luokkia

--- Sali3/Program.cs
+++ Sali3/Program.cs
@@ -22,14 +22,26 @@ namespace Sali3
 
             // Testataan Jäsen-olion muodostimen toiminta
             Jasen jasen = new Jasen("Mika", "Vainio", "Yläneentie 146", "23100", "Mynämäki", "123", "1");
-            
-            // Katsotaan olion ominaisuuksia
+
+            // Testataan Trainer-olion muodostimen toiminta, huom. nimiavaruus Henkilosto
+            Henkilosto.Trainer trainer = new Henkilosto.Trainer("Jakke", "Jäynä", "Kotikatu 2", "21200", "Raisio", "Juoksumatto", "044 7057425", "jakke@huuhaa.com");
+
+            // Testatataan Punnitus-olion toimintaa
+
+            Punnitus punnitus = new Punnitus("10.9.2020", "70,5", "1,71");
+
+            // Katsotaan olioiden ominaisuuksia
             Console.WriteLine("Testi oletusmuodostimelle");
             henkilo.naytaHenkilo();
             Console.WriteLine("Testi muodostimelle kaikilla param");
             henkilo1.naytaHenkilo();
             Console.WriteLine("Testi jäsenen muodostimelle");
             jasen.naytaJasen();
+            Console.WriteLine("Testi personal trainerin muodostimelle");
+            trainer.naytaTrainer();
+            Console.WriteLine("Testi punnituksen muodostimelle");
+            punnitus.naytaPunnitus();
+            Console.WriteLine("Painoindeksi (BMI-metodi) on " + punnitus.BMI());
         }
     }
 }
